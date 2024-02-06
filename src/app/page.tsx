09a847d0ac54ec0ab783d322e5d941ad3d344d78
Maxim <maxhard243@gmail.com>
2024-02-06 12:41:57 +0300

    feat: add animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,11 @@ import Header from '../components/Header';
 import { Canvas } from "@react-three/fiber";
 import { LogoModel } from '../components/LogoModel';
 import React from 'react';
+import { OrbitControls } from '@react-three/drei';
 
 export default function Home() {
-  const { rotation } = useSpring({ from: {rotation: 0}, to: {rotation: Math.PI}, loop: true, delay: 1000})
-  console.log(rotation);
+  const { rotation } = useSpring({ from: {rotation: 1}, to: {rotation: Math.PI * 2}, loop: true, delay: 1000})
+
   return (
     <Container maxWidth="xl">
       <Header />
@@ -20,11 +21,12 @@ export default function Home() {
       </Grid2>
       <div className={'h-[90vh]'}>
         <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 4] }}>
-          <ambientLight intensity={3} />
-          <pointLight position={[0, 0, 3]} intensity={10} />
           <animated.group rotation-y={rotation}>
             <LogoModel />
           </animated.group>
+          <directionalLight position={[0, 0, 3]} />
+          <ambientLight intensity={3} />
+          <pointLight position={[0, 0, 3]} intensity={20} />
         </Canvas>
       </div>
     </Container>
